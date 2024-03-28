d4f2e12f20f6fdbf3190c7b499fcef8aa2045198
Snikimonkd <cepminkis132@gmail.com>
2024-03-28 18:34:54 +0300

    add command key

--- qmk/keymaps/default/keymap.c
+++ qmk/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC , KC_GRV , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , /**/ KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
                      KC_TAB , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , /**/ KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
                      KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , /**/ KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_ENT,
-                                       KC_LCTL, KC_LALT, NUMB   , KC_SPC , /**/ KC_BSPC, ARROWS , KC_ENT , KC_ESC
+                                       KC_LCTL, KC_LALT, NUMB   , KC_SPC , /**/ KC_BSPC, ARROWS , KC_LCMD, KC_ESC
     ),
     [_NUMBERS] = LAYOUT(
             _______, _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, /**/ KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,
--- qmk/keymaps/vial/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x0E, 0x77, 0xBB, 0x5B, 0x48, 0x66, 0xF4, 0x94}
-#define VIAL_UNLOCK_COMBO_ROWS {0,0}
-#define VIAL_UNLOCK_COMBO_COLS {0,1}
